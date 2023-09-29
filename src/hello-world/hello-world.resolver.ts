1e7887f405fae228882dd1fea4d7d1c6056f18ff
Miguel Lupani <mlupani2@gmail.com>
2023-09-29 17:48:22 -0300

    fin seccion 2

---
+++ src/hello-world/hello-world.resolver.ts
@@ -0,0 +1,31 @@
+import { Args, Float, Int, Query, Resolver } from '@nestjs/graphql';
+
+@Resolver()
+export class HelloWorldResolver {
+  @Query(() => String, { name: 'hello', description: 'retorna hola mundo' })
+  helloWorld() {
+    return 'Hola mundo!';
+  }
+
+  @Query(() => Float, {
+    name: 'randomNumber',
+    description: 'retorna un numero aleatorio',
+  })
+  getRandomNumber(): number {
+    return Math.random() * 100;
+  }
+
+  @Query(() => Int, {
+    name: 'randomFromZeroTo',
+    description: 'retorna un numero aleatorio entre 0 y 10',
+  })
+  getRandomFromZeroTo(
+    @Args('to', {
+      type: () => Int,
+      nullable: true,
+    })
+    to: number = 6,
+  ): number {
+    return Math.floor(Math.random() * to);
+  }
+}
